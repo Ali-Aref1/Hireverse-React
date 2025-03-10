f3e5553ff50403d39a2cd9059fedacac76a70913
Ali-Aref1 <ali14915@gmail.com>
2025-03-10 13:57:39 +0200

    css fix for background opacity

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Interview } from './pages/Interview'
 
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
-import {Home} from './pages/Home'
-import {Interview} from './pages/Interview'
 function App() {
-
   return (
     <>
-      <div className="w-screen h-screen bg-primary z-0 relative text-white">
+      <div className="w-screen h-screen bg-primary z-0 relative text-white bg-base">
         <Router>
           <Routes>
             <Route path="/" element={<Home />} />
